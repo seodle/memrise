b8e6877f1efe9cc26c86a6c76129494efb8877ed
Sunny <sunny.avry@gmail.com>
2020-01-10 18:07:26 +0100

    Add files via upload

---
+++ text_to_speech.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from gtts import gTTS
+import os
+
+# Define the language
+
+language = "en"
+
+# import your word from a text file
+
+myTextImport = open("memrise_words.txt", "r")
+
+for line in myTextImport:
+
+	myText = myTextImport.readline().replace("\n", "") #replace line breals by space
+
+	# Create output
+
+	output = gTTS(text = myText, lang = language, slow = False) #slow = False for normal speech rate
+
+	# Export audio files
+
+	output.save(myText+".mp3")
+
+# Close the file
+myTextImport.close()
+
+
+
+
